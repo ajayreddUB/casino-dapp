7a1da9cd7f19c86bfa3183024090ae63007bdec8
Sairam Gurram <sairam.gurram1234@gmail.com>
2023-11-18 15:15:03 -0500

    Updated HTML,CSS,JS

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-document.getElementById('rollButton').addEventListener('click', function() {
-    let bet = parseInt(document.getElementById('bet').value);
-    let balanceElement = document.getElementById('balance');
-    let balance = parseInt(balanceElement.textContent.replace('Balance: $', ''));
+// Function to generate a random number between 1 and six
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * 6) + 1;
+};
 
-    if (!bet || bet < 0 || bet > balance) {
-        alert('Please enter a valid bet amount.');
+// Function to create the image path for the dice images
+const generateImgPath = (imgNum) => {
+    return `images/dice${imgNum}.png`
+};
+
+// Play button handler for the game logic
+$("#play").click(function() {
+    const betValue = parseInt($('#bet').val());
+    const desiredOutcome = parseInt($('#desiredOutcome').val());
+
+    // Bet value validation
+    if (betValue <= 0) {
+        alert('Please enter a valid amount for the bet.');
         return;
     }
 
-    let diceResult = Math.ceil(Math.random() * 6);
-    document.getElementById('dice').textContent = `Dice: ${diceResult}`;
-    let win = diceResult > 3; // Example win condition
-
-    if (win) {
-        balance += bet;
-        document.getElementById('result').textContent = 'You win!';
-    } else {
-        balance -= bet;
-        document.getElementById('result').textContent = 'You lose!';
+    // Desired outcome validation
+    if (desiredOutcome < 1 || desiredOutcome > 6) {
+        alert('Please enter a valid desired outcome (1-6).');
+        return;
     }
 
-    balanceElement.textContent = `Balance: $${balance}`;
+    // Roll the dice and set the image
+    const rolledNumber = getRandomNumber();
+    const diceImg = generateImgPath(rolledNumber);
+    document.querySelector(".img1").setAttribute("src", diceImg);
+
+    // Check the outcome
+    let resultText = '';
+    if (rolledNumber === desiredOutcome) {
+        resultText = 'Congratulations! You guessed correctly!';
+    } else {
+        resultText = 'Sorry, try again!';
+    }
+
+    $("h1").text(resultText);
 });
 
